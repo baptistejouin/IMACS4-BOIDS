a9dcb8114df8ec6332bcf2167495f1d5ee7d161d
EliottDvx <eliottdevaux@gmail.com>
2024-03-18 17:00:11 +0100

    ⚗️ new boids default params

--- src/Boids/boidsParams.hpp
+++ src/Boids/boidsParams.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
 struct BoidsParams {
-    float turnFactor{0.11f};
-    float visualRange{.2f};
-    float protectedRange{.06f};
-    float centeringFactor{.005f};
-    float avoidFactor{.5f};
-    float matchingFactor{.5f};
-    float maxSpeed{.3f};
-    float minSpeed{.1f};
+    float turnFactor{0.03f};
+    float visualRange{.15f};
+    float protectedRange{.065f};
+    float centeringFactor{.001f};
+    float avoidFactor{.9f};
+    float matchingFactor{.02f};
+    float maxSpeed{.8f};
+    float minSpeed{.5f};
 };
